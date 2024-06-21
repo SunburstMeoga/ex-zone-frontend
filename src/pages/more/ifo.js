052b924104059221307584999dfae08455c1ee1a
SunburstMeoga <1025113986@qq.com>
2024-06-21 17:40:59 +0800

    ifo

--- src/pages/more/ifo.js
+++ src/pages/more/ifo.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { ifoState, ifoSaleAttributes, publicSaleItems,ifoDetailsItems } from '@/dictionary/more'
+import { ifoState, ifoSaleAttributes, publicSaleItems, ifoDetailsItems } from '@/dictionary/more'
 const Ifo = () => {
     let [currentIFOState, changeIFOState] = useState(1)
     let handleIFOState = (id) => {
@@ -25,10 +25,17 @@ const Ifo = () => {
                     </div>
                 </div>
                 <div className='w-full flex flex-col justify-start items-center gradient-voting-banner '>
-                    <div className='w-full rounded-t-3xl overflow-hidden'>
+                    {currentIFOState === 1 && <div className='w-full rounded-t-3xl overflow-hidden mb-2-2'>
                         <img className='' src='/images/phone/ifo-tiger.png' alt=''></img>
-                    </div>
-                    <div className='w-22-0 text-white text-2-0 font-bold line-height-point-133 mt-2-2 mb-0-8'>
+                    </div>}
+                    {currentIFOState === 2 && <div className='text relative mb-4-7'>
+                        <div className='w-5-1 h-5-1 rounded-full flex justify-center items-center bg-menu-green text-white -bottom-2-5 left-9-5 absolute' >
+                            <div className='icon iconfont icon-down' style={{ fontSize: '2rem'}}></div>
+                        </div>
+                        <div className='w-full rounded-3xl border-menu-green border-8 overflow-hidden'>
+                        <img className='' src='/images/phone/ifo-tiger.png' alt=''></img>
+                    </div></div>}
+                    <div className='w-22-0 text-white text-2-0 font-bold line-height-point-133  mb-0-8'>
                         Public Sale
                     </div>
                     <div>
@@ -101,15 +108,38 @@ const Ifo = () => {
                             <span className='text-menu-green text-1-2 '>Lista DAO</span> is a liquid staking and decentralized stablecoin protocol. Users can liquid stake their BNB to receive slisBNB, and borrow lisUSD against accepted collateral assets.
                         </div>
                     </div>
-                    <div className='w-full h-3-5 bg-primary-purple flex justify-center items-center rounded-b-3xl text-menu-green'>
+                    {currentIFOState === 1 && <div className='w-full h-3-5 bg-primary-purple flex justify-center items-center rounded-b-3xl text-menu-green'>
                         <div className='text-1-5 font-semibold mr-1-0'>Details</div>
                         <div className='flex justify-center items-center'>
                             <div className='icon iconfont icon-down'></div>
                         </div>
-                    </div>
+                    </div>}
                 </div>
 
-                <div className='gradient-voting-banner w-22-0 h-21-1 rounded-xl mt-1-5 flex flex-col justify-start items-center py-2-1 mb-1-5'>
+                {currentIFOState === 2 && <div className='text relative  mt-2-7 mb-5-1'>
+                        <div className='w-5-1 h-5-1 rounded-full flex justify-center items-center bg-menu-green text-white -bottom-2-5 left-9-5 absolute' >
+                            <div className='icon iconfont icon-down' style={{ fontSize: '2rem'}}></div>
+                        </div>
+                        <div className='w-full rounded-3xl border-menu-green border-8 overflow-hidden'>
+                        <img className='' src='/images/phone/ifo-tiger.png' alt=''></img>
+                    </div></div>}
+                    {currentIFOState === 2 && <div className='text relative  mb-5-1'>
+                        <div className='w-5-1 h-5-1 rounded-full flex justify-center items-center bg-menu-green text-white -bottom-2-5 left-9-5 absolute' >
+                            <div className='icon iconfont icon-down' style={{ fontSize: '2rem'}}></div>
+                        </div>
+                        <div className='w-full rounded-3xl border-menu-green border-8 overflow-hidden'>
+                        <img className='' src='/images/phone/ifo-tiger.png' alt=''></img>
+                    </div></div>}
+                    {currentIFOState === 2 && <div className='text relative  mb-5-1'>
+                        <div className='w-5-1 h-5-1 rounded-full flex justify-center items-center bg-menu-green text-white -bottom-2-5 left-9-5 absolute' >
+                            <div className='icon iconfont icon-down' style={{ fontSize: '2rem'}}></div>
+                        </div>
+                        <div className='w-full rounded-3xl border-menu-green border-8 overflow-hidden'>
+                        <img className='' src='/images/phone/ifo-tiger.png' alt=''></img>
+                    </div></div>}
+
+                {currentIFOState === 1 && <div>
+                    <div className='gradient-voting-banner w-22-0 h-21-1 rounded-xl mt-1-5 flex flex-col justify-start items-center py-2-1 mb-1-5'>
                     <div className='w-19-0 flex justify-between items-center mb-2-7'>
                         <div className='text-white text-1-5'>My HAH</div>
                         <div className='flex justify-end items-center'>
@@ -131,78 +161,78 @@ const Ifo = () => {
                     </div>
                     <div className='text-white bg-primary-purple w-19-0 h-4-7 rounded-xl flex justify-center items-center text-1-5 font-bold mb-1-0 mt-1-0'>Connect Wallet</div>
                 </div>
-
-                <div className='w-22-0 flex justify-between items-center text-1-5 mb-2-8'>
-                    <div className='text-menu-green underline'>My HAH</div>
-                    <div className='text-white'>1x</div>
-                </div>
-
-                <div className='gradient-voting-banner w-22-0  rounded-3xl flex flex-col justify-start items-center mb-1-7 relative py-2-0'>
-                    <div className='w-19-0 flex flex-col justify-start items-center text-white'>
-                        <div className='text-2-0 text-white font-bold mb-0-5 w-full'>
-                            Token Vesting
-                        </div>
-                        <div className='text-1-2 font-medium line-height-point-133 w-full mb-3-4 text-white'>
-                            You have no tokens <br></br> available for claiming
-                        </div>
-                        <div className='w-12-8 mb-2-2'>
-                            <img src='/images/phone/empty.png'></img>
-                        </div>
-                        <div className='font-bold text-1-2 w-full mb-1-0'>
-                            You have no tokens <br></br> available for claiming
-                        </div>
-                        <div className='w-full text-1-0 mb-3-1'>
-                            Participate in our next IFO. and remember to lock your HAH to increase your allocation!
-                        </div>
-                        <div className='text-white bg-primary-purple w-full h-4-7 rounded-xl flex justify-center items-center text-1-5 font-bold mb-1-0 mt-1-0'>Connect Wallet</div>
-                        <div className='text-menu-green underline text-1-5 font-bold'>How does it work?</div>
+                    <div className='w-22-0 flex justify-between items-center text-1-5 mb-2-8'>
+                        <div className='text-menu-green underline'>My HAH</div>
+                        <div className='text-white'>1x</div>
                     </div>
-                </div>
 
-                <div className='bg-cover bg-center bg-no-repeat bg-ifo-banner-one w-22-0 h-83-6 rounded-3xl flex flex-col justify-start items-center py-2-7 mb-1-7'>
-                    <div className='font-bold text-2-0 line-height-point-133 text-white w-19-0 mb-3-7'>
-                        How to Take Part <br></br> in the Public Sale
-                    </div>
-                    <div className='flex flex-col justify-start items-center'>
-                        {publicSaleItems.map((item, index) => {
-                            return <div key={index} className='mb-1-8'>
-                                <div className='flex justify-start items-center w-19-0 text-white mb-0-5'>
-                                    <div className='w-5-1 h-5-1 rounded-full bg-ifo-border flex justify-center items-center shadow-2xl'>
-                                        <div className='bg-ifo-round flex justify-center items-center text-white text-2-2 w-4-1 h-4-1 rounded-full font-medium'>{item.id}</div>
-                                    </div>
-                                    <div className='text-1-5 w-11-8 ml-2-1 font-bold'>{item.title}</div>
-                                </div>
-                                <div className='flex justify-between items-center w-19-0 text-white '>
-                                    <div className={`w-1-4 ml-1-9 bg-ifo-round mt-0-4 ${item.height}`}></div>
-                                    <div className='line-height-point-133 text-0-9 w-11-8'>{item.content}</div>
-                                </div>
+                    <div className='gradient-voting-banner w-22-0  rounded-3xl flex flex-col justify-start items-center mb-1-7 relative py-2-0'>
+                        <div className='w-19-0 flex flex-col justify-start items-center text-white'>
+                            <div className='text-2-0 text-white font-bold mb-0-5 w-full'>
+                                Token Vesting
                             </div>
-                        })}
-                    </div>
-                </div>
-                <div className='w-full relative flex flex-col justify-start items-center' style={{ height: '118.3rem' }}>
-                    <div className='bg-cover bg-center bg-no-repeat bg-ifo-details-banner w-full absolute -top-0-1' style={{ height: '118.3rem' }}></div>
-                    <div className='z-10 relative flex flex-col justify-start items-center'>
-                        <div className='text-2-0 font-bold text-white mb-2-2'>Details</div>
-                    </div>
-                    <div className='rounded-3xl w-22-0 text-white z-10 relative'>
-                        <div className='w-full rounded-t-3xl bg-primary-purple px-3-1 py-1-7 text-1-2 line-height-point-181 font-medium'>
-                            What are the sale types? What are the differences between them?
+                            <div className='text-1-2 font-medium line-height-point-133 w-full mb-3-4 text-white'>
+                                You have no tokens <br></br> available for claiming
+                            </div>
+                            <div className='w-12-8 mb-2-2'>
+                                <img src='/images/phone/empty.png'></img>
+                            </div>
+                            <div className='font-bold text-1-2 w-full mb-1-0'>
+                                You have no tokens <br></br> available for claiming
+                            </div>
+                            <div className='w-full text-1-0 mb-3-1'>
+                                Participate in our next IFO. and remember to lock your HAH to increase your allocation!
+                            </div>
+                            <div className='text-white bg-primary-purple w-full h-4-7 rounded-xl flex justify-center items-center text-1-5 font-bold mb-1-0 mt-1-0'>Connect Wallet</div>
+                            <div className='text-menu-green underline text-1-5 font-bold'>How does it work?</div>
                         </div>
-                        <div className='w-full rounded-b-3xl bg-ifo-details px-3-1 py-1-7 line-height-point-217 font-medium mb-2-5'>
-                            Your iHAH number for each IFOs is calculated based on your veHAH balance at the snapshot time of each IFOs. Usually the snapshot time is the end time of each IFOs. Therefore, iHAH can varies between different IFOs.
-                            To get more iHAH, simply get more veHAH by locking more HAH in your veHAH position, or extending your veHAH position.
+                    </div>
+
+                    <div className='bg-cover bg-center bg-no-repeat bg-ifo-banner-one w-22-0 h-83-6 rounded-3xl flex flex-col justify-start items-center py-2-7 mb-1-7'>
+                        <div className='font-bold text-2-0 line-height-point-133 text-white w-19-0 mb-3-7'>
+                            How to Take Part <br></br> in the Public Sale
                         </div>
                         <div className='flex flex-col justify-start items-center'>
-                        {ifoDetailsItems.map((item, index) => {
-                            return <div key={index} className='bg-primary-purple text-white text-1-2 w-22-0 h-8-6 rounded-3xl mb-2-5 line-height-point-180  px-1-7 flex items-center'>
-                                { item.title}
+                            {publicSaleItems.map((item, index) => {
+                                return <div key={index} className='mb-1-8'>
+                                    <div className='flex justify-start items-center w-19-0 text-white mb-0-5'>
+                                        <div className='w-5-1 h-5-1 rounded-full bg-ifo-border flex justify-center items-center shadow-2xl'>
+                                            <div className='bg-ifo-round flex justify-center items-center text-white text-2-2 w-4-1 h-4-1 rounded-full font-medium'>{item.id}</div>
+                                        </div>
+                                        <div className='text-1-5 w-11-8 ml-2-1 font-bold'>{item.title}</div>
+                                    </div>
+                                    <div className='flex justify-between items-center w-19-0 text-white '>
+                                        <div className={`w-1-4 ml-1-9 bg-ifo-round mt-0-4 ${item.height}`}></div>
+                                        <div className='line-height-point-133 text-0-9 w-11-8'>{item.content}</div>
+                                    </div>
+                                </div>
+                            })}
+                        </div>
+                    </div>
+                    <div className='w-full relative flex flex-col justify-start items-center' style={{ height: '118.3rem' }}>
+                        <div className='bg-cover bg-center bg-no-repeat bg-ifo-details-banner w-full absolute -top-0-1' style={{ height: '118.3rem' }}></div>
+                        <div className='z-10 relative flex flex-col justify-start items-center'>
+                            <div className='text-2-0 font-bold text-white mb-2-2'>Details</div>
+                        </div>
+                        <div className='rounded-3xl w-22-0 text-white z-10 relative'>
+                            <div className='w-full rounded-t-3xl bg-primary-purple px-3-1 py-1-7 text-1-2 line-height-point-181 font-medium'>
+                                What are the sale types? What are the differences between them?
                             </div>
-                        })}
+                            <div className='w-full rounded-b-3xl bg-ifo-details px-3-1 py-1-7 line-height-point-217 font-medium mb-2-5'>
+                                Your iHAH number for each IFOs is calculated based on your veHAH balance at the snapshot time of each IFOs. Usually the snapshot time is the end time of each IFOs. Therefore, iHAH can varies between different IFOs.
+                                To get more iHAH, simply get more veHAH by locking more HAH in your veHAH position, or extending your veHAH position.
+                            </div>
+                            <div className='flex flex-col justify-start items-center'>
+                                {ifoDetailsItems.map((item, index) => {
+                                    return <div key={index} className='bg-primary-purple text-white text-1-2 w-22-0 h-8-6 rounded-3xl mb-2-5 line-height-point-180  px-1-7 flex items-center'>
+                                        {item.title}
+                                    </div>
+                                })}
+                            </div>
+                        </div>
+
                     </div>
-                    </div>
-                    
-                </div>
+                </div>}
 
             </div>
 
