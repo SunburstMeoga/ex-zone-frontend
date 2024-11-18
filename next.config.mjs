8c4fe5d8a8ea4273a6a7389718f9183d99639b65
SunburstMeoga <1025113986@qq.com>
2024-11-18 23:02:57 +0800

    feat

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,19 @@ const nextConfig = {
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // 修改 Terser 配置，保留 console.log
+      config.optimization.minimizer = config.optimization.minimizer.map((plugin) => {
+        if (plugin.constructor.name === 'TerserPlugin') {
+          plugin.options.terserOptions.compress.drop_console = false;
+        }
+        return plugin;
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
